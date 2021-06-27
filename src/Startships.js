c23282455518787ecb5516527a3be0444fad3ff1
Consecratio <jcasique@gmail.com>
2021-06-26 21:42:03 -0700

    finished initial build, needs styling and second part of bonus

---
+++ src/Startships.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Starships = () => {
+    const [starships, setStarships] = useState([])
+    const [apiUrl, setApiUrl] = useState('https://swapi.dev/api/starships/?page=1')
+    
+    useEffect(() => {
+        if(apiUrl !== null){
+            axios.get(apiUrl)
+                .then(res => {
+                    setStarships(starships.concat(res.data.results))
+                    setApiUrl(res.data.next)
+                    console.log(res.data.results)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }, [apiUrl])
+
+    const shipNames = starships.map((ship, index) => {
+        return (
+            <div key={index}>
+                <p>Name: {ship.name}</p>
+                <p>Class: {ship.starship_class}</p>
+            </div>
+        )
+    })
+ 
+    return(
+        <div>
+            {shipNames}
+        </div>
+    )
+}
+
+export default Starships
